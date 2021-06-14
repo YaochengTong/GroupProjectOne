c9dba8e3ebffe96dfe09a431425b2866fa38ee24
tjfy1992 <zhongqiu_gao@126.com>
2021-06-14 17:11:10 -0400

    modified:   auth-server/group-project-auth/src/main/java/com/app/groupprojectauth/controller/UserController.java

--- auth-server/group-project-auth/src/main/java/com/app/groupprojectauth/controller/UserController.java
+++ auth-server/group-project-auth/src/main/java/com/app/groupprojectauth/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.app.groupprojectauth.controller;
 
+import com.app.groupprojectauth.domain.User;
+import com.app.groupprojectauth.security.JwtUtil;
 import com.app.groupprojectauth.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,9 @@ public class UserController {
     @Autowired
     private IUserService iUserService;
 
+    private static final String jwtTokenCookieName = "JWT-TOKEN";
+    private static final String signingKey = "signingKey";
+
     @PostMapping("/login")
     public Map<String, Object> login(@RequestParam Map<String, Object> params){
         if(params.get("username") == null || params.get("password") == null){
@@ -23,7 +28,11 @@ public class UserController {
             resultMap.put("reason", "Invalid username or password for login");
             return resultMap;
         }
-        return iUserService.userLogin(params);
+        Map<String, Object> map =  iUserService.userLogin(params);
+        User user = (User) map.get("user");
+        String token = JwtUtil.generateToken(signingKey, user.getUsername());
+        map.put(jwtTokenCookieName, token);
+        return map;
     }
 
     @PostMapping("/register")
