40e247a6739a2ab12d6c024f4d5b1ad6f532ff34
YaochengTong <tongyaocheng@gmail.com>
2021-06-14 20:54:59 -0400

    Added Authentication for login, if user clear cache from browser, it will autmatically log out of the system

--- frontend/HRProjectAngular/src/app/app-routing.module.ts
+++ frontend/HRProjectAngular/src/app/app-routing.module.ts
@@ -4,12 +4,25 @@ import { EmployeeModule } from './employee/employee.module';
 import { HumanResourceModule } from './human-resource/human-resource.module';
 import { LoginModule } from './login/login.module';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
   { path: 'login', loadChildren: () => LoginModule },
-  { path: 'employee', loadChildren: () => EmployeeModule },
-  { path: 'human-resource', loadChildren: () => HumanResourceModule },
+  {
+    path: 'employee',
+    loadChildren: () => EmployeeModule,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'human-resource',
+    loadChildren: () => HumanResourceModule,
+    canActivate: [AuthGuard],
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- frontend/HRProjectAngular/src/app/app.module.ts
+++ frontend/HRProjectAngular/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexModule } from '@angular/flex-layout';
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,6 +25,7 @@ import { FlexModule } from '@angular/flex-layout';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CoreModule,
     FlexModule,
     MatButtonModule,
     MatToolbarModule,
