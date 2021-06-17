8f8351e41aa83050739a6434c6159b04cee52dba
YaochengTong <tongyaocheng@gmail.com>
2021-06-17 01:49:44 -0400

    modified: housing backend - housing controller frontend - housing-detail.component pages

--- application-server/group-project-application/src/main/java/com/app/groupprojectapplication/service/impl/HouseServiceImpl.java
+++ application-server/group-project-application/src/main/java/com/app/groupprojectapplication/service/impl/HouseServiceImpl.java
@@ -4,9 +4,13 @@ import com.app.groupprojectapplication.dao.IHouseDao;
 import com.app.groupprojectapplication.domain.House;
 import com.app.groupprojectapplication.service.IHouseService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HouseServiceImpl implements IHouseService {
 
+    @Autowired
     IHouseDao iHouseDao;
 
     @Override
