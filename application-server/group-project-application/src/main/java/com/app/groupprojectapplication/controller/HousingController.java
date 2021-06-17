0ff9b94e5cda9913d779f1faeb3cb171b2ccc241
YaochengTong <tongyaocheng@gmail.com>
2021-06-17 14:28:46 -0400

    modified: Housing controller

--- application-server/group-project-application/src/main/java/com/app/groupprojectapplication/controller/HousingController.java
+++ application-server/group-project-application/src/main/java/com/app/groupprojectapplication/controller/HousingController.java
@@ -1,5 +1,6 @@
 package com.app.groupprojectapplication.controller;
 
+import com.app.groupprojectapplication.dao.IHouseDao;
 import com.app.groupprojectapplication.domain.House;
 import com.app.groupprojectapplication.service.IHouseService;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,11 @@ public class HousingController {
     @Autowired
     private IHouseService iHouseService;
 
-    @GetMapping("/all")
+
+    @Autowired
+    private IHouseDao iHouseDao;
+
+    @GetMapping("/get-houses")
     public Map<String, Object> postAllHouse() {
         Map<String, Object> resultMap = new HashMap<>();
         List<House> houses = iHouseService.getAllHouse();
@@ -28,6 +34,14 @@ public class HousingController {
         System.out.println(houses);
         return resultMap;
 
+    }
+    @GetMapping("/get-house/{houseId}")
+    public Map<String, Object> getHouseById(@PathVariable Integer houseId) {
+        Map<String, Object> resultMap = new HashMap<>();
+        House h = iHouseDao.getHouseById(houseId);
+        System.out.println(h.toString());
+        resultMap.put("house",h);
+        return resultMap;
     }
 
 }
