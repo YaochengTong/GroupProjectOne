38581e5c0f155c539749561d8ca64e5396a913cc
YaochengTong <tongyaocheng@gmail.com>
2021-06-17 19:22:48 -0400

    modified: HousePageInfo backend

--- application-server/group-project-application/src/main/java/com/app/groupprojectapplication/service/impl/HouseServiceImpl.java
+++ application-server/group-project-application/src/main/java/com/app/groupprojectapplication/service/impl/HouseServiceImpl.java
@@ -1,16 +1,11 @@
 package com.app.groupprojectapplication.service.impl;
 
 import com.app.groupprojectapplication.dao.IHouseDao;
-import com.app.groupprojectapplication.domain.FacilityReport;
-import com.app.groupprojectapplication.domain.House;
 import com.app.groupprojectapplication.domain.HouseElement.HousePageInfo;
-import com.app.groupprojectapplication.domain.HouseElement.ReportDetail;
 import com.app.groupprojectapplication.service.IHouseService;
 import java.util.List;
-import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class HouseServiceImpl implements IHouseService {
@@ -24,31 +19,43 @@ public class HouseServiceImpl implements IHouseService {
     }
 
     @Override
-    @Transactional
     public List<HousePageInfo> getAllHouse() {
-        List<House> houseList = iHouseDao.getAllHouse();
-        for (House h : houseList) {
-            HousePageInfo hpi = new HousePageInfo();
-            hpi.setHouseId(h.getId());
-            hpi.setAddress(h.getAddress());
-            hpi.setLandlord(generateRandomLandlord());
-        }
-        return houseList;
+        return null;
     }
 
-    public String generateRandomLandlord() {
-        const String allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        StringBuilder sb = new StringBuilder(5);
-        for(int i =0; i < 5;i++){
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            sb.append(allChar.charAt(index));
-        }
-        return sb.toString();
-    }
+    //@Override
+    //@Transactional
+    //public List<HousePageInfo> getAllHouse() {
+    //    List<House> houseList = iHouseDao.getAllHouse();
+    //    for (House h : houseList) {
+    //        HousePageInfo hpi = new HousePageInfo();
+    //        hpi.setHouseId(h.getId());
+    //        hpi.setAddress(h.getAddress());
+    //        hpi.setLandlord(generateRandomLandlord());
+    //        Set<Employee> eList = h.getEmployeeSet();
+    //        Set<Facility> fList = h.getFacilitySet();
+    //        List<HouseEmployeeInfo> houseEmployeeInfoList = new ArrayList<>();
+    //        List<HouseFacilityInfo> houseFacilityInfoList = new ArrayList<>();
+    //    }
+    //    return houseList;
+    //}
 
-    public ReportDetail getReportDetailFromReport(FacilityReport fr) {
-        ReportDetail rd = new ReportDetail();
-        return rd;
-    }
+    //public String generateRandomLandlord() {
+    //    const String allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //    StringBuilder sb = new StringBuilder(5);
+    //    for (int i = 0; i < 5; i++) {
+    //        int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+    //        sb.append(allChar.charAt(index));
+    //    }
+    //    return sb.toString();
+    //}
 
+    //public ReportDetail getReportDetailFromReport(FacilityReport fr) {
+    //    ReportDetail rd = new ReportDetail();
+    //    return rd;
+    //}
+    //
+    //public List<HouseEmployeeInfo> transferEmployeeList(Set<Employee> eList) {
+    //     List<HouseEmployeeInfo> houseEmployeeInfoList
+    //}
 }
