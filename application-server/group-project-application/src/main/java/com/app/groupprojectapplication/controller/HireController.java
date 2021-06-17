8651cdceede024b86d609859ca0d9d88939cf2d7
tjfy1992 <zhongqiu_gao@126.com>
2021-06-16 23:07:49 -0400

    modified:   application-server/group-project-application/src/main/java/com/app/groupprojectapplication/controller/HireController.java 	modified:   application-server/group-project-application/src/main/java/com/app/groupprojectapplication/domain/ApplicationWorkflow.java

--- application-server/group-project-application/src/main/java/com/app/groupprojectapplication/controller/HireController.java
+++ application-server/group-project-application/src/main/java/com/app/groupprojectapplication/controller/HireController.java
@@ -1,10 +1,17 @@
 package com.app.groupprojectapplication.controller;
 
+import com.app.groupprojectapplication.file.AmazonS3FileService;
 import com.app.groupprojectapplication.service.IHireService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @CrossOrigin
@@ -15,6 +22,9 @@ public class HireController {
     @Autowired
     private IHireService iHireService;
 
+    @Autowired
+    private AmazonS3FileService amazonS3FileService;
+
     @PostMapping("/generateToken")
     public Map<String, Object> generateToken(@RequestParam Map<String, Object> params){
         Map<String, Object> resultMap = new HashMap<>();
@@ -22,6 +32,52 @@ public class HireController {
         Integer userId = Integer.parseInt(params.get("user_id").toString());
         iHireService.generateAToken(email, userId);
         resultMap.put("result", "success");
+        return resultMap;
+    }
+
+    @PostMapping("/submitOnboard")
+    public Map<String, Object> submitOnboardApplication(@RequestParam Map<String, Object> paramMap,
+                                                      HttpServletRequest request){
+        Map<String, Object> resultMap = new HashMap<>();
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request)
+                .getFiles("file");
+        Integer userId = Integer.parseInt(paramMap.get("user_id").toString());
+
+        /**
+         * Step 1: deal with the form
+         * Step 1.1: create a person
+         * Step 1.2 create one person for each contact
+         * step 1.3 insert into contact table
+         * step 1.4 insert into application workflow
+         */
+
+
+
+        /**
+         * Step 2: File uploads
+         * Step 2.1: deal with the file upload
+         * Step 2.2: insert into personal document table
+         */
+        for (int i = 0; i < files.size(); ++i) {
+            InputStream ips = null;
+            try {
+                ips = files.get(i).getInputStream();
+                String result = amazonS3FileService.upload(ips, userId + "/"
+                        + files.get(i).getOriginalFilename());
+                System.out.println(result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if(ips != null){
+                    try {
+                        ips.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
         return resultMap;
     }
 }
