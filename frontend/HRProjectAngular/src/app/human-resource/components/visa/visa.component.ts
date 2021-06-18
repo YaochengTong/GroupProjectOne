c8c1a638f0a55a144b9f120fdab71a2b0192a7e2
XinyeBai4 <xinye.bai4@gmail.com>
2021-06-18 05:34:55 -0400

    frontend stylying

--- frontend/HRProjectAngular/src/app/human-resource/components/visa/visa.component.ts
+++ frontend/HRProjectAngular/src/app/human-resource/components/visa/visa.component.ts
@@ -19,7 +19,7 @@ export class VisaComponent implements OnInit {
 
   isDataAvailable: boolean = true;
   isEdit: boolean = false;
-  // visaStatusInfo: any = {};
+  visaStatusInfo: any = {};
   columnsToDisplay = ['Full Name', 'Work Authorization', 'Expiration Date', 'Day Left'];
   expandedElement!: VisaStatusElement | null;
   map = {
@@ -30,82 +30,82 @@ export class VisaComponent implements OnInit {
   }
   ;
 
-  visaStatusInfo = [
-    {
-        "fullName": "David Luo",
-        "workAuthorization": "F1(OPT/CPT)",
-        "authorizationStartDate": "2020-12-02",
-        "authorizationEndDate": "2022-09-23",
-        "authorizationDayLeft": 462,
-        "documentReceived": [
-            "I-983.txt",
-            "OPT_EAD.txt",
-            "OPT_Receipt.txt"
-        ],
-        "nextStep": "I-20 after I-983 Submitted",
-        "idx": 0,
-        "userId": 89
-    },
-    {
-        "fullName": "Admin Admin",
-        "workAuthorization": "F1(OPT/CPT)",
-        "authorizationStartDate": "2021-06-14",
-        "authorizationEndDate": "2021-12-23",
-        "authorizationDayLeft": 188,
-        "documentReceived": [
-            "I-20.txt",
-            "I-983.txt",
-            "OPT_EAD.txt",
-            "OPT_Receipt.txt",
-            "OPT_STEM_EAD.txt",
-            "OPT_STEM_Receipt.txt"
-        ],
-        "nextStep": "No Further Action Needed",
-        "idx": 1,
-        "userId": 556
-    },
-    {
-        "fullName": "Ricard  Huang",
-        "workAuthorization": "F1(OPT/CPT)",
-        "authorizationStartDate": "2021-06-14",
-        "authorizationEndDate": "2021-09-23",
-        "authorizationDayLeft": 97,
-        "documentReceived": [
-            "I-20.txt",
-            "I-983.txt",
-            "OPT_EAD.txt",
-            "OPT_Receipt.txt"
-        ],
-        "nextStep": "OPT STEM Receipt",
-        "idx": 2,
-        "userId": 557
-    },
-    {
-        "fullName": "Bailey Bai",
-        "workAuthorization": "F1(OPT/CPT)",
-        "authorizationStartDate": "2021-06-14",
-        "authorizationEndDate": "2021-09-23",
-        "authorizationDayLeft": 97,
-        "documentReceived": [
-            "OPT_EAD.txt",
-            "OPT_Receipt.txt"
-        ],
-        "nextStep": "I-983 for OPT STEM",
-        "idx": 3,
-        "userId": 558
-    }
-]
+//   visaStatusInfo = [
+//     {
+//         "fullName": "David Luo",
+//         "workAuthorization": "F1(OPT/CPT)",
+//         "authorizationStartDate": "2020-12-02",
+//         "authorizationEndDate": "2022-09-23",
+//         "authorizationDayLeft": 462,
+//         "documentReceived": [
+//             "I-983.txt",
+//             "OPT_EAD.txt",
+//             "OPT_Receipt.txt"
+//         ],
+//         "nextStep": "I-20 after I-983 Submitted",
+//         "idx": 0,
+//         "userId": 89
+//     },
+//     {
+//         "fullName": "Admin Admin",
+//         "workAuthorization": "F1(OPT/CPT)",
+//         "authorizationStartDate": "2021-06-14",
+//         "authorizationEndDate": "2021-12-23",
+//         "authorizationDayLeft": 188,
+//         "documentReceived": [
+//             "I-20.txt",
+//             "I-983.txt",
+//             "OPT_EAD.txt",
+//             "OPT_Receipt.txt",
+//             "OPT_STEM_EAD.txt",
+//             "OPT_STEM_Receipt.txt"
+//         ],
+//         "nextStep": "No Further Action Needed",
+//         "idx": 1,
+//         "userId": 556
+//     },
+//     {
+//         "fullName": "Ricard  Huang",
+//         "workAuthorization": "F1(OPT/CPT)",
+//         "authorizationStartDate": "2021-06-14",
+//         "authorizationEndDate": "2021-09-23",
+//         "authorizationDayLeft": 97,
+//         "documentReceived": [
+//             "I-20.txt",
+//             "I-983.txt",
+//             "OPT_EAD.txt",
+//             "OPT_Receipt.txt"
+//         ],
+//         "nextStep": "OPT STEM Receipt",
+//         "idx": 2,
+//         "userId": 557
+//     },
+//     {
+//         "fullName": "Bailey Bai",
+//         "workAuthorization": "F1(OPT/CPT)",
+//         "authorizationStartDate": "2021-06-14",
+//         "authorizationEndDate": "2021-09-23",
+//         "authorizationDayLeft": 97,
+//         "documentReceived": [
+//             "OPT_EAD.txt",
+//             "OPT_Receipt.txt"
+//         ],
+//         "nextStep": "I-983 for OPT STEM",
+//         "idx": 3,
+//         "userId": 558
+//     }
+// ]
   constructor(
     private httpRequestService: HTTPReq,
   ) {}
 
   ngOnInit(): void {
-    // this.httpRequestService.getData('/visaStatusManagement/all', null, 'http://localhost:8999').subscribe(
-    //   (data: any) => {
-    //     this.isDataAvailable = true;
-    //     this.visaStatusInfo = data.visaStatusInfoList;
-    //   }
-    // )
+    this.httpRequestService.getData('/visaStatusManagement/all', null, 'http://localhost:8999').subscribe(
+      (data: any) => {
+        this.isDataAvailable = true;
+        this.visaStatusInfo = data.visaStatusInfoList;
+      }
+    )
   }
 
   @ViewChild('fullName') fullName!: ElementRef;
