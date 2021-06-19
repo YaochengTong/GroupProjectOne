267539cefc017d34badf4f0f1fd10396abdd2321
tjfy1992 <zhongqiu_gao@126.com>
2021-06-19 03:05:46 -0400

    modified:   application-server/group-project-application/src/main/java/com/app/groupprojectapplication/dao/impl/ApplicationWorkflowDaoImpl.java 	modified:   application-server/group-project-application/src/main/java/com/app/groupprojectapplication/service/impl/HireServiceImpl.java 	modified:   frontend/HRProjectAngular/src/app/human-resource/components/hire/details-dialog/details-dialog.component.html 	modified:   frontend/HRProjectAngular/src/app/human-resource/components/hire/details-dialog/details-dialog.component.scss 	modified:   frontend/HRProjectAngular/src/app/human-resource/components/hire/details-dialog/details-dialog.component.ts 	modified:   frontend/HRProjectAngular/src/app/human-resource/components/hire/hire.component.ts

--- frontend/HRProjectAngular/src/app/human-resource/components/hire/details-dialog/details-dialog.component.ts
+++ frontend/HRProjectAngular/src/app/human-resource/components/hire/details-dialog/details-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-details-dialog',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsDialogComponent implements OnInit {
 
-  constructor() { }
+  applicationObj: any = {};
+
+  email: string = "test@gmail.com";
+
+  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: {application: any}) { 
+    this.applicationObj = data.application;
+    let datePipe: DatePipe = new DatePipe('en-US');
+    this.applicationObj.documents.forEach( item => {
+      if(item.createDate){
+        item.createDate = datePipe.transform(item.createDate, 'MM/dd/yyyy');
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  personalInfoForm = this.fb.group({
+    
+  })
+
+  contactInfoForm = this.fb.group({
+    
+  })
+
+  fileForm = this.fb.group({
+    
+  })
 
 }
