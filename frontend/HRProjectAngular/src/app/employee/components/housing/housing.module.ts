fc1ac8d0380847a7e9bdf2a01ee6a3e256a2a7d7
YaochengTong <tongyaocheng@gmail.com>
2021-06-20 02:35:31 -0400

    Housing Module bug fixed

--- frontend/HRProjectAngular/src/app/employee/components/housing/housing.module.ts
+++ frontend/HRProjectAngular/src/app/employee/components/housing/housing.module.ts
@@ -9,17 +9,16 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
-import {MatListModule} from "@angular/material/list";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FacilityReportComponent } from './facility-report/facility-report.component';
-import { TestformComponent } from './testform/testform.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HousingPageComponent, FacilityReportComponent, TestformComponent],
+  declarations: [HousingPageComponent, FacilityReportComponent],
   imports: [
     CommonModule,
     HousingRoutingModule,
