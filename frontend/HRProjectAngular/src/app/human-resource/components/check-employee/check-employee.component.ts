9a88e70dd64c811d95e7933cb4533ca6f259c2ba
simonshengsssd <35588599+simonshengsssd@users.noreply.github.com>
2021-06-20 16:40:08 -0700

    modified: works with error

--- frontend/HRProjectAngular/src/app/human-resource/components/check-employee/check-employee.component.ts
+++ frontend/HRProjectAngular/src/app/human-resource/components/check-employee/check-employee.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { HTTPReq } from 'src/app/service/HTTPReq/HTTPReq.service';
 import { Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
 import { MatTableDataSource } from '@angular/material/table';
 
 
@@ -13,28 +12,6 @@ export interface PeriodicElement {
   visaStatus: string;
 }
 
-export interface PElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-
-
 @Component({
   selector: 'app-check-employee',
   templateUrl: './check-employee.component.html',
@@ -43,7 +20,7 @@ const ELEMENT_DATA: PElement[] = [
 
 
 
-export class CheckEmployeeComponent implements OnInit, AfterViewInit {
+export class CheckEmployeeComponent implements OnInit {
 
   public dataSource;
   public len!: number;
@@ -54,6 +31,7 @@ export class CheckEmployeeComponent implements OnInit, AfterViewInit {
     'startingDate',
     'visaStatus',
     'position',
+    'details'
   ];
 
 
@@ -71,20 +49,17 @@ export class CheckEmployeeComponent implements OnInit, AfterViewInit {
     )
   }
 
-
-
-  ngOnInit(): void {
-    // this.httpRequestService.getData('/profile/all', null, 'http://localhost:8999').subscribe(
-    //   (data:any) => {
-    //     this.isDataAvailable = true;
-    //     this.dataSource = new MatTableDataSource<PeriodicElement>(data.AllProfile);
-    //     this.len = Object.keys(data.AllProfile).length;
-    //   }
-    // )
-    // this.dataSource.paginator = this.paginator;
+  details(employeeId : number) {
+    alert(employeeId);
   }
 
+
   @ViewChild(MatPaginator) paginator! : MatPaginator;
+  ngOnInit(): void {
+    console.log(this.isDataAvailable);
+  }
+
+  
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
